bd5a843083b1f8bf68d5ea7564a4a6b99e83077b
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-19 16:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	 let radius = document.getElementById('radius').value;
+
+	if (radius === '' || radius < 0) {
+                alert("Please enter a valid non-negative number for the radius.");
+                return;
+            }
+	radius = parseFloat(radius);
+	let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed(4);
+	document.getElementById("volume").value=volume;
   
 } 
 
